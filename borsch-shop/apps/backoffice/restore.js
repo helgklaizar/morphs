6ff18aece899ff0f99f279b9989ed024eb278789
Helg Klaizar <klaizar@gmail.com>
2026-04-19 17:59:25 +0300

    Delete borsch-shop directory

--- borsch-shop/apps/backoffice/restore.js
+++
@@ -1,7 +0,0 @@
-const sqlite3 = require('sqlite3').verbose();
-const fs = require('fs');
-const path = require('path');
-
-const SUPABASE_URL = "http://35.252.3.177:8000";
-// We don't have the anon key easily, let's use psql on the VPS to dump to a JSON file, or we can use the gcloud command inside node.
-
